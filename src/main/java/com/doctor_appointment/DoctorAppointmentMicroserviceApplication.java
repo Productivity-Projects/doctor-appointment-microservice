b410a1430fd61c4953c91f9593fa90e906bddc0e
satya <satyaprasanth56@gmail.com>
2024-02-11 14:55:45 +0530

    1st commit

---
+++ src/main/java/com/doctor_appointment/DoctorAppointmentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.doctor_appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAppointmentMicroserviceApplication.class, args);
+	}
+
+}
